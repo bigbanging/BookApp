7d007a52f24af6c2ee5786abda3dd93739f07776
gwdragon <litter_k@163.com>
2018-08-12 16:28:09 +0800

    make tag

--- app/src/main/java/com/bigbang/bookapp/BookActivity.java
+++ app/src/main/java/com/bigbang/bookapp/BookActivity.java
@@ -4,26 +4,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bigbang.bookapp.view.BookPageBezierHelper;
 import com.bigbang.bookapp.view.BookPageView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author: litte
  * Created on 2018/8/11  17:35
  * Project Name:BookApp
  * Package Name:com.bigbang.bookapp
- * Description: TODO
+ * Description: 阅读视图
  * Copyright (c) 2018, gwl All Rights Reserved.
  */
 public class BookActivity extends AppCompatActivity {
@@ -32,6 +42,8 @@ public class BookActivity extends AppCompatActivity {
     private BookPageView mBookPageView;
     private TextView mTv_progress;
     private BookPageBezierHelper mBookPageBezierHelper;
+    private RelativeLayout mRelativeLayout;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,6 +57,9 @@ public class BookActivity extends AppCompatActivity {
         // init view
         mBookPageView = findViewById(R.id.bookPageView);
         mTv_progress = findViewById(R.id.tv_progress);
+        mRelativeLayout = findViewById(R.id.setting);
+        mRecyclerView = findViewById(R.id.recyclerView);
+
         //get width height
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -66,6 +81,21 @@ public class BookActivity extends AppCompatActivity {
                 mTv_progress.setText(currentLength/totalLength+"%");
             }
         });
+        mBookPageView.setOnUserNeedSettingListener(new BookPageView.OnUserNeedSettingListener() {
+            @Override
+            public void onUserNeedSetting() {
+                mRelativeLayout.setVisibility(mRelativeLayout.getVisibility() == View.VISIBLE ? View.GONE:View.VISIBLE);
+            }
+        });
+        List<String> mData = new ArrayList<>();
+        mData.add("添加书签");
+        mData.add("读取书签");
+        mData.add("设置背景");
+        mData.add("语音朗读");
+        mData.add("跳转进度");
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(new MyRecyclerViewAdapter(this,mData));
         Intent intent = getIntent();
         if (intent != null) {
             String filePath = intent.getStringExtra(FILE_PATH);
@@ -88,5 +118,44 @@ public class BookActivity extends AppCompatActivity {
         Intent intent = new Intent(context, BookActivity.class);
         intent.putExtra(FILE_PATH, filePath);
         context.startActivity(intent);
+    }
+
+    private class MyRecyclerViewAdapter extends RecyclerView.Adapter {
+        private Context mContext;
+        private List<String> mData = new ArrayList<>();
+        MyRecyclerViewAdapter(Context context, List<String> mData) {
+            mContext = context;
+            this.mData = mData;
+        }
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            TextView textView = new TextView(mContext);
+            return new ViewHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            TextView textView = (TextView) holder.itemView;
+            textView.setWidth(240);
+            textView.setHeight(180);
+            textView.setTextSize(26);
+            textView.setTextColor(Color.RED);
+            textView.setGravity(Gravity.CENTER);
+            textView.setText(mData.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            private TextView mTextView;
+            public ViewHolder(TextView itemView) {
+                super(itemView);
+                mTextView = itemView;
+            }
+        }
     }
 }
